e37920a4d9965aac907112dc38c93c3d395a7102
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-06-21 19:15:41 +1000

    Add global styles to remove body margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import { css } from "@emotion/core";
+import { css, Global } from "@emotion/core";
 
 import avatar from '../images/avatar.jpg';
 
@@ -15,28 +15,38 @@ export default function Home({ data }) {
   `;
 
   return (
-    <header
-      css={css`
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          top: 0;
-          left: 0;
-          margin: 0;
-          background-color: ${bgColor}
+    <div>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
         `}
-    >
-      <div css={css`padding: 15px; color: ${fgColor}`}>
-        mdwyer
-      </div>
-      <nav css={css`
-        display: flex;
-        justify-content: flex-end;
-      `}>
-        <Link to="/about" css={navItemStyle}>About</Link>
-        <Link to="/contact" css={navItemStyle}>Contact</Link>
-        <Link to="/blog" css={navItemStyle}>Blog</Link>
-      </nav>
-    </header>
+      />
+      <header
+        css={css`
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            top: 0;
+            left: 0;
+            margin: 0;
+            background-color: ${bgColor}
+          `}
+      >
+        <div css={css`padding: 15px; color: ${fgColor}`}>
+          mdwyer
+        </div>
+        <nav css={css`
+          display: flex;
+          justify-content: flex-end;
+        `}>
+          <Link to="/about" css={navItemStyle}>About</Link>
+          <Link to="/contact" css={navItemStyle}>Contact</Link>
+          <Link to="/blog" css={navItemStyle}>Blog</Link>
+        </nav>
+      </header>
+    </div>
+
   );
 }
