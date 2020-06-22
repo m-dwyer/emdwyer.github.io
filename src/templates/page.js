e4696642061d75f06f7b88c300c57b7d4850e18b
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-06-22 15:57:44 +1000

    Remove extension from import

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/layout.js";
+import Layout from "../components/layout";
 import { graphql } from "gatsby";
 
 export default function Page({ data }) {
