86295d3924fa04b50ae164ad65bbe06e10c84d5f
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-06-24 12:08:31 +1000

    Add button with scroll to for new section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,48 @@
 import React from "react";
+import { css } from "@emotion/core";
 import Layout from "../components/layout";
+import { FaArrowDown } from "react-icons/fa";
 
-export default function Home({ data }) {
-  return (
-    <Layout>
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.separator = React.createRef();
+
+    this.scrollToContent = this.scrollToContent.bind(this);
+  }
+
+  scrollToContent() {
+    this.separator.current.scrollIntoView({ block: "start", behavior: "smooth" });
+  }
+
+  render() {
+    return (
+      <Layout>
       <section>
         <h1>Hi, I'm mdwyer</h1>
+        <button
+          css={css`
+            border: 0;
+            border-radius: 50%;
+            height: 80px;
+            width: 80px;
+            cursor: pointer;
+          `}
+          onClick={this.scrollToContent}
+        >
+          <FaArrowDown
+            size={60}
+            css={css`
+            `}
+          />
+        </button>
       </section>
-    </Layout>    
-  );
+      <section ref={this.separator}>
+      </section>
+    </Layout>  
+    );
+  }
 }
+
+export default Index;
