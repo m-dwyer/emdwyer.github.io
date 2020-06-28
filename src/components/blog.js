dd83a55b7ac3a26813029e9926f9ccec252ba0a2
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-06-28 17:49:35 +1000

    Add link to blog post page

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { css } from "@emotion/core";
 import { Link } from "gatsby";
+import { FaCalendar, FaArrowRight } from "react-icons/fa";
 
 export default function Blog(props) {
   return (
@@ -13,30 +14,60 @@ export default function Blog(props) {
       </h1>
       {props.posts.map(({childMarkdownRemark: p}) => {
         return (
-          <article key={p.fields.slug}>
-            <Link to={p.fields.slug} css={css`
-                  color: inherit;
-                  text-decoration: none;
-                `}>
-              <header>
-                <h3>
-                  {p.frontmatter.title}
-                </h3>
-                <small>
-                  {p.frontmatter.date}
-                </small>
-              </header>
-              <section>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: p.frontmatter.description || p.excerpt
-                  }}
-                />
-              </section>
-            </Link>
-          </article>
+          <React.Fragment>
+            <article
+              className="blog-preview"
+              key={p.fields.slug}
+              css={css`
+                display: flex;
+                justify-content: space-between;
+                width: 60vw;
+              `}>
+              <Link to={p.fields.slug} css={css`
+                    color: inherit;
+                    text-decoration: none;
+                  `}>
+                <header>
+                  <h3>
+                    {p.frontmatter.title}
+                  </h3>
+                  <small>
+                    <FaCalendar />  {p.frontmatter.date}
+                  </small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: p.frontmatter.description || p.excerpt
+                    }}
+                  />
+                </section>
+              </Link>
+            </article>
+            <hr />
+          </React.Fragment>
         );
       })}
+      <div>
+        <Link className="link" to="/posts/">
+          <span>More</span>
+          <button
+                css={css`
+                  border: 0;
+                  border-radius: 50%;
+                  height: 20px;
+                  width: 20px;
+                  cursor: pointer;
+                `}
+              >
+                <FaArrowRight
+                  size={10}
+                  css={css`
+                  `}
+                />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ src/pages/posts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+import { css } from "@emotion/core";
+
+export default function Posts() {
+  return (
+    <Layout>
+    </Layout>
+  )
+}
