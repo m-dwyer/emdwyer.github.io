7553ae43d86568d6cabdd84068b37befc0df8c4e
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-07-03 20:19:45 +1000

    Slightly refactor styling for nav bar items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,24 +3,24 @@ import { Link } from "gatsby";
 import { css } from "@emotion/core";
 
 export default function Layout({ children }) {
-  const navItemStyle = theme => css`
-    padding: 24px;
-    text-decoration: none;
-    color: ${theme.colors.fgColor};
-    &:hover {
-      color: ${theme.colors.fontColor};
-    }
-`;
-
   return (
     <nav>
-      <ul css={css`
+      <ul css={theme => css`
         display: flex;
         justify-content: flex-end;
         list-style-type: none;
+
+        li .nav-item {
+          padding: 24px;
+          text-decoration: none;
+          color: ${theme.colors.fgColor};
+          &:hover {
+            color: ${theme.colors.fontColor};
+          }
+        }
       `}>
-        <li><Link to="/" css={navItemStyle}>Home</Link></li>
-        <li><Link to="/blog"css={navItemStyle}>Blog</Link></li>
+        <li><Link to="/" className="nav-item">Home</Link></li>
+        <li><Link to="/blog" className="nav-item">Blog</Link></li>
       </ul>
     </nav>
   );
