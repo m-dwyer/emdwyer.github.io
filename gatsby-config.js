a2904a3f04214d671a68779704ba15120c103181
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2020-07-13 18:49:28 +1000

    Add second blog post and gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,19 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 600
+            }
+          }
+        ]
+      }
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
