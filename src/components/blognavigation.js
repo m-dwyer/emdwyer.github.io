4565627f725d163c031638db3a78c12a2cda1f4f
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-03-22 12:19:37 +1100

    Remove unused FluidContainer import

--- src/components/blognavigation.js
+++ src/components/blognavigation.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { css } from "@emotion/core"
 import { Link } from "gatsby"
-import FluidContainer from "./fluidcontainer"
-
 const BlogNavigation = ({ previous, next }) => {
   return (
     <div
