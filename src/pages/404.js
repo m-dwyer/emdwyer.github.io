b3bd5bd0b8d1d500cecfc6597fedc0312fffba7e
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-03-28 16:57:35 +1100

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import FluidContainer from "../components/fluidcontainer"
+
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <FluidContainer>
+        <h1>Whoops..</h1>
+        <p>
+          It looks like you tried to access a route that doesn&#39;t exist..
+          Ouch.
+        </p>
+        <p>
+          You should maybe consider <Link to="/">heading home</Link>
+        </p>
+      </FluidContainer>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
